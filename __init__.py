0e2c1b4a59eef1bf0393206b8bbb81647476d0f7
Allan Barcellos <scanning@capellamarket.com>
2020-02-12 11:18:03 -0800

    Totalpass p600

--- __init__.py
+++
@@ -1,9 +0,0 @@
-"""
-SDK for IconTime TotalPass P600 Timeclocks
-"""
-from .api import TimeClockApi
-from .report import TimeClockReport
-from .punches import Punches, Punch
-from .employees import Employee, Employees
-
-__all__ = [TimeClockApi, TimeClockReport, Punches, Punch, Employees, Employee]
